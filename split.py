db2efd9c20dd4a61755b214a52821a7e30a3838d
Roman Eisner <roman.eisner@edmonton.ca>
2018-03-21 09:01:04 -0600

    splitting and saving works.

--- split.py
+++ split.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 from pydub import AudioSegment
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--min',
@@ -8,29 +9,37 @@ parser.add_argument('--min',
                     required = True,
                     help = 'minutes per chunk.')
 parser.add_argument('--file',
-                    type = argparse.FileType('r'),
                     required = True,
                     help = 'mp3 file to split.')
 
 args = parser.parse_args()
 min_per_chunk = args.min
 input_file = args.file
+output_file_prefix = re.sub('\.mp3$', '', input_file) + "_"
 
 print("Splitting into " + str(min_per_chunk) + " min chunks")
+print("Reading: " + input_file)
 
 segment = AudioSegment.from_mp3(input_file)
-segment_length_in_sec = len(segment)
+segment_length_in_sec = len(segment) / 1000
 
-print("Length of file: " + len(segment_length_in_sec))
+print("Length of file: " + str(segment_length_in_sec) + "sec")
 
-sec_per_chunk = min_per_chunk * 60
-
+sec_per_chunk = min_per_chunk #* 60
 num_chunks = math.ceil(segment_length_in_sec / float(sec_per_chunk))
 
 if num_chunks > 1:
-    print("Splitting into " + num_chunks + " chunks.")
+    print("Splitting into " + str(num_chunks) + " chunks.")
     for i in range(int(num_chunks)):
-        chunk = segment[i * sec_per_chunk:(i + 1) * sec_per_chunk]
-        # first_half.export("blah.mp3", format="mp3")
+        start = i * sec_per_chunk
+        end = (i + 1) * sec_per_chunk
+        chunk = segment[start * 1000 : end * 1000]
+        output_file = output_file_prefix + "%03d" % (i + 1) + ".mp3"
+        print("     ... Saving from " +
+              str(start) + " to " + str(end) +
+              " -> " + output_file)
+        chunk.export(output_file, format = "mp3")
 else:
     raise SystemExit('File is not long enough!')
+
+print("Done!")
