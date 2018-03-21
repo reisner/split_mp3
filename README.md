a3ef7070d6159ce44391c3953d47cd371d5fe847
Roman Eisner <roman.eisner@edmonton.ca>
2018-03-20 21:19:37 -0600

    testing

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # split_mp3
-Split Mp3 file into smaller chunks
+Split Mp3 file into smaller chunks.
+
+## Usage
+
+```
+python split.py --min 20 --file ~/The_unsolved_mysteries_of_cream_cheese.mp3
+```
+
+## Setup
+
+Requires [pydub](https://github.com/jiaaro/pydub) to be setup correctly. Something like this works on OSX:
+
+```
+# libav
+brew install libav --with-libvorbis --with-sdl --with-theora
+
+####    OR    #####
+
+# ffmpeg
+brew install ffmpeg --with-libvorbis --with-sdl2 --with-theora
+```
--- split.py
+++ split.py
@@ -1,12 +1,43 @@
 import argparse
+import math
+from pydub import AudioSegment
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--min',
                     type = int,
                     required = True,
                     help = 'minutes per chunk.')
+parser.add_argument('--file',
+                    type = argparse.FileType('r'),
+                    required = True,
+                    help = 'mp3 file to split.')
 
 args = parser.parse_args()
 min_per_chunk = args.min
+input_file = args.file
 
 print("Splitting into " + str(min_per_chunk) + " min chunks")
+
+segment = AudioSegment.from_mp3(input_file)
+segment_length_in_sec = len(segment)
+
+print("Length of file: " + len(segment_length_in_sec))
+
+sec_per_chunk = min_per_chunk * 60
+
+num_chunks = math.ceil(segment_length_in_sec / float(sec_per_chunk))
+#     return [audio_segment[i * chunk_length:(i + 1) * chunk_length]
+#             for i in range(int(number_of_chunks))]
+
+if num_chunks > 1:
+    print("Splitting into " + num_chunks + " chunks.")
+else:
+    raise SystemExit('File is not long enough!')
+
+#:45
+
+# halfway_point = len(sound) // 2
+# first_half = sound[:halfway_point]
+#
+# # create a new file "first_half.mp3":
+# first_half.export("/path/to/first_half.mp3", format="mp3")
